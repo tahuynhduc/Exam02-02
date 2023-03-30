c345f11e2dcee1c7dcaa58ec027a8ab9db91ccef
tahuynhduc <tahduc03032000@gmail.com>
2023-03-30 21:08:36 +0700

    exam02.v2

---
+++ IEnemy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IEnemy.IEnemy; 
+
+namespace IEnemy
+{
+    public enum Enemy_type
+    {
+        GOBLIN,
+        BOSS,
+        ORC,
+    }
+    public enum Attack_type
+    {
+        RANGE,
+        MELEE,
+    }
+
+    interface IEnemy
+    {
+        long Id { get; set; }
+        string Name { get; set; }
+        Enemy_type Type { get; set; }
+        int Health { get; set; }
+        Attack_type AttackType { get; set; }
+        float Damage { get;}
+        void ShowInfo();
+    }
+    public class Enemy : IEnemy
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+
+        public float Damage { get; }
+
+        public Enemy_type Type { get; set; }
+        public Attack_type AttackType { get; set ; }
+
+        public void ShowInfo()
+        {
+            throw new NotImplementedException();
+
+            Console.WriteLine("id:" +Id);
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Health:" + Health);
+            Console.WriteLine("Damage:" + Damage);
+            Console.WriteLine("Type" +Type);
+            Console.WriteLine("AttackType" + AttackType);
+
+
+        }
+        public List<float> DamageList = new List<float>();
+
+        public float this[int i]
+        {
+            get { return DamageList[i]; }
+            set { DamageList[i] = value; }
+        }
+
+        public void CalDamage()
+        {
+            int max = 0;
+            float Damage = 0;
+            foreach (int i in DamageList)
+            {
+                if(Damage < DamageList[i])
+                {
+                    Damage = max;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IEnemy.IEnemy;
+
+using IEnemy;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+using System.IO;
+
+namespace exam2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Enemy enemy = new Enemy();
+
+            Hashtable openWith = new Hashtable();
+            openWith.Add(enemy.Id, enemy);
+
+            Console.WriteLine("—----------------------------------------—----------------------------------------");
+            Console.WriteLine("1.Insert new enemy…");
+            Console.WriteLine("2.Display all the enemy list…");
+            Console.WriteLine("3.Calculate the enemy damage…");
+            Console.WriteLine("4.Find enemy");
+            Console.WriteLine("5.Exit.");
+            Console.WriteLine("—----------------------------------------—----------------------------------------");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    //Console.WriteLine("Input enemy Id:");
+                    //enemy.Id = long.Parse(Console.ReadLine());
+                    //Console.WriteLine("Input enemy name:");
+                    //enemy.Name = Console.ReadLine();
+                    //Console.WriteLine("Input enemy type:");
+                    //enemy.Type = (Enemy_type)int.Parse(Console.ReadLine());
+                    //Console.WriteLine("Input enemy health:");
+                    //enemy.Health = int.Parse(Console.ReadLine());
+                    //Console.WriteLine("Input enemy AttackType:");
+                    //enemy.AttackType = (Attack_type)int.Parse(Console.ReadLine());
+                    //Console.WriteLine("Input enemy damages list count:");
+
+                    foreach (float aPart in DamageList)
+                    {
+                        Console.WriteLine(aPart);
+                    }
+
+
+
+                    Console.Write("Input damage 1:");
+                    float DamageList1 = int.Parse(Console.ReadLine());
+                    enemy[0] = DamageList1;
+                    float DamageList2 = int.Parse(Console.ReadLine());
+                    enemy[1] = DamageList2;
+                    float DamageList3 = int.Parse(Console.ReadLine());
+                    enemy[2] = DamageList3;
+                    
+
+                    break;
+                case 2:
+                    foreach (var item in openWith)
+                    {
+                        Console.WriteLine(openWith);
+                    }
+                    foreach (DictionaryEntry de in openWith)
+                    {
+                        Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+                    }
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    return;
+            }
+        }
+    }
+}
